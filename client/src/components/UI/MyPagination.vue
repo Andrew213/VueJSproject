b534f3012be525e0af56632b4181533e277c644a
Andrey Kochanov <kochanov@lanbilling.ru>
2024-01-30 13:04:38 +0300

    Добавил компонент пагинации

---
+++ client/src/components/UI/MyPagination.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import { PropType, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "my-pagination",
+  props: {
+    page: {
+      type: Number as PropType<number>,
+      required: true,
+    },
+    totalPages: {
+      type: Number as PropType<number>,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick(pageNumber: number) {
+      this.$emit("page-click", pageNumber);
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="pagination">
+    <my-button
+      class="pagination__btn"
+      v-for="pageNumber in totalPages"
+      :key="pageNumber"
+      :class="{
+        pagination__current: pageNumber === page,
+      }"
+      @click="handleClick(pageNumber)"
+    >
+      {{ pageNumber }}
+    </my-button>
+  </div>
+</template>
+
+<style scoped>
+.pagination {
+  margin: 20px;
+  /* display: flex;
+  justify-content: space-around; */
+  align-items: center;
+}
+
+.pagination__btn + .pagination__btn {
+  margin-left: 20px;
+}
+
+.pagination__current {
+  border: 1px solid teal;
+  background-color: transparent;
+  color: teal;
+}
+</style>
--- client/src/components/UI/index.ts
+++ client/src/components/UI/index.ts
@@ -2,5 +2,6 @@ import MyButton from "./MyButton.vue";
 import MyInput from "./MyINput.vue";
 import MyModal from "./MyModal.vue";
 import MySelect from "./MySelect.vue";
+import MyPagination from "./MyPagination.vue";
 
-export default [MyButton, MyInput, MyModal, MySelect];
+export default [MyButton, MyInput, MyModal, MySelect, MyPagination];
